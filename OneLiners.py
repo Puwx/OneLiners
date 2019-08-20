dfa96b8915f0595336033658ec7dee39c1c91035
Matthew Kemper <37680606+Puwx@users.noreply.github.com>
2019-08-20 09:37:08 -0700

    Changed variable names for consistency

--- OneLiners.py
+++ OneLiners.py
@@ -4,7 +4,7 @@ import arcpy
 import pandas as pd
 
 #Create a Pandas DataFrame out of a ArcGIS feature class - must have env set to dir containing feature class to convert.
-fcToDf = lambda x: pd.DataFrame([r for r in arcpy.da.SearchCursor(x,'*')],columns=[f.name for f in arcpy.ListFields(x)])
+FCtoDF = lambda x: pd.DataFrame([r for r in arcpy.da.SearchCursor(x,'*')],columns=[f.name for f in arcpy.ListFields(x)])
 
 #Find all of the files in a directory (fp) if they contain a string of interest (soi) - works recursively
 findFiles = lambda fp,soi: [os.path.join(r,f) for r,_,fs in os.walk(fp) for f in fs if soi in f]
