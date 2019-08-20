ea032d355692c612d7be7b82b14b3d27d3bd060a
Matthew Kemper <37680606+Puwx@users.noreply.github.com>
2019-08-20 09:36:11 -0700

    Added the first set of one-liners
    I don't recommend using any of these.

---
+++ OneLiners.py
@@ -0,0 +1,15 @@
+import os
+import arcpy
+
+import pandas as pd
+
+#Create a Pandas DataFrame out of a ArcGIS feature class - must have env set to dir containing feature class to convert.
+fcToDf = lambda x: pd.DataFrame([r for r in arcpy.da.SearchCursor(x,'*')],columns=[f.name for f in arcpy.ListFields(x)])
+
+#Find all of the files in a directory (fp) if they contain a string of interest (soi) - works recursively
+findFiles = lambda fp,soi: [os.path.join(r,f) for r,_,fs in os.walk(fp) for f in fs if soi in f]
+
+#Convert a DMS string seperated by spaces e.g. 51 43 19.533 to a DD value e.g. 51.722093
+DMStoDD = lambda row: sum([x/y for x, y in zip([float(x) for x in row[:-1].split(' ')], [1, 60, 3600])]) * (-1 if row[-1] in ('S', 'W') else 1)
+
+#More to come...
