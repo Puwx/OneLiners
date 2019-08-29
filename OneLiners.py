c547f8a4bd6e4365ff26578b7f696ba4a4c1f405
Matthew Kemper <37680606+Puwx@users.noreply.github.com>
2019-08-28 19:50:36 -0700

    Update OneLiners.py

--- OneLiners.py
+++ OneLiners.py
@@ -1,9 +1,11 @@
 import os
 import arcpy
 
+from collections import Counter
+
 import pandas as pd
 
-#Create a Pandas DataFrame out of a ArcGIS feature class - must have env set to dir containing feature class to convert.
+#Create a Pandas DataFrame out of a ArcGIS feature class table - must have env set to dir containing feature class to convert.
 FCtoDF = lambda x: pd.DataFrame([r for r in arcpy.da.SearchCursor(x,'*')],columns=[f.name for f in arcpy.ListFields(x)])
 
 #Find all of the files in a directory (fp) if they contain a string of interest (soi) - works recursively
@@ -11,5 +13,8 @@ findFiles = lambda fp,soi: [os.path.join(r,f) for r,_,fs in os.walk(fp) for f in
 
 #Convert a DMS string seperated by spaces e.g. 51 43 19.533N to a DD value e.g. 51.722093
 DMStoDD = lambda row: sum([x/y for x, y in zip([float(x) for x in row[:-1].split(' ')], [1, 60, 3600])]) * (-1 if row[-1] in ('S', 'W') else 1)
+
+#Get the number of occurences for each of the values found in 'field' in the feature class 'fc'.
+fieldValCounter = lambda fc,field: dict(Counter([str(row[0]).encode('ascii') for row in arcpy.da.SearchCursor(fc,field)])
 
 #More to come...
